6fd8ed9b6c37198e6059a20ed28f6e4ba27adc88
NewGyu <newgyu+git@kinoboku.net>
2022-06-19 01:03:58 +0000

    ADD:wasm-gen attr

--- ffi-example/src/lib.rs
+++ ffi-example/src/lib.rs
@@ -1,10 +1,14 @@
 use std::ffi::{CStr, CString};
+#[cfg(all(target_arch = "wasm32", not(target_os = "wasi")))]
+use wasm_bindgen::prelude::*;
 
+#[cfg_attr(all(target_arch = "wasm32", not(target_os = "wasi")), wasm_bindgen)]
 pub fn print_str(s: &str) {
     let cstring = CString::new(s).unwrap();
     unsafe { ffi_example_sys::print_str(cstring.as_ptr()) }
 }
 
+#[cfg_attr(all(target_arch = "wasm32", not(target_os = "wasi")), wasm_bindgen)]
 pub fn hello() -> String {
     unsafe { CStr::from_ptr(ffi_example_sys::hello()) }
         .to_str()
